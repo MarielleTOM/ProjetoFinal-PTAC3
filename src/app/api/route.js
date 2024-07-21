e66857d53d139bc2b6ec9b174c9780c769bebffd
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2024-07-21 14:03:06 -0300

    21/07

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,20 +3,62 @@ import { NextResponse } from "next/server";
 export const listaDeBolsas = [
     {
         id: 1,
-        titulo: "O Senhor dos Anéis",
-        autor: "J.R.R. Tolkien",
-        anoPublicacao: 1954,
-        genero: "Fantasia"
+        img: 'https://www.melissa.com.br/ccstore/v1/images/?source=/file/v2235123694688338482/products/cor.34454.34454BD92211_o.jpg&height=940&width=940',
+        Bolsa: "Melissa X Telfar Medium Jelly Shopper II",
+        Cor: "Transparente",
+        Preço: "R$" + 449.90,
+        Tamanho: "Unc"
     },
     {
         id: 2,
-        titulo: "1984",
-        autor: "George Orwell",
-        anoPublicacao: 1949,
-        genero: "Distopia"
-    }
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
+    {
+        id: 3,
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
+    {
+        id: 4,
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
+    {
+        id: 5,
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
+    {
+        id: 6,
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
+    {
+        id: 7,
+        img: '',
+        Bolsa: "",
+        Cor: "",
+        Preço: "R$" + ,
+        Tamanho: "Unc"
+    },
 ];
 
 export async function GET(){
     return NextResponse.json(listaDeBolsas)
-}
+}https://www.melissa.com.br/ccstore/v1/images/?source=/file/v3549990850390973753/products/cor.34451.34451BD38310_o.jpg&height=940&width=940
